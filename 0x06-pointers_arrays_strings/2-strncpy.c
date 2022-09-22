521b0646965a0b3c982dd01984d85887070c96de
Wahome-2nd <sheillawahome@gmail.com>
2022-09-22 14:14:55 -0700

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncpy - is a function that copies a string.
+ *
+ * @src: the source string that would be appended to dest
+ *
+ * @dest: the string that would be concatenated
+ *
+ * @n: is a number
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
